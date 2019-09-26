a376fe38437064495f588138390367624861acf6
JC Lenz <jc@miserablepop.com>
2019-09-25 21:22:49 -0700

    Updated game.js to include function for checking what the user selected.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
     // VARIABLES
     ///////////////////////////////////////////////////////////////////////////////
 
-   
+    // Defining the questions, choices, and answers
+
     var questions = ['The Simpsons live on the following street...'];
     var firstChoice = ['Woodview Terrace'];
     var secondChoice = ['Pine Tree Terrace'];
@@ -11,12 +12,18 @@ $(document).ready(function(){
     var fourthChoice = ['Evergreen Terrace'];
     var answer = ['Evergreen Terrace'];
     
-    
+    // Defining the counters for the end game
+
     var correct = 0;
     var incorrect = 0;
     var unanswered = 0;
     
+
     var isSelected = false;
+
+
+    // Defining the timer variables
+
     var count = 0;
     var timer;
     var time = 30;
@@ -25,7 +32,8 @@ $(document).ready(function(){
     // FUNCTIONS
     ///////////////////////////////////////////////////////////////////////////////
     
-    
+    // Show the question and choices
+
     function showGameContainers() {
         $("#question").show();
         $("#choice-1").show();
@@ -33,6 +41,9 @@ $(document).ready(function(){
         $("#choice-3").show();
         $("#choice-4").show();
     }
+
+    // Hide the question and choices
+
     function hideGameContainers() {
         $("#question").hide();
         $("#choice-1").hide();
@@ -40,6 +51,9 @@ $(document).ready(function(){
         $("#choice-3").hide();
         $("#choice-4").hide();
     }
+
+    // Hide the final results 
+
     function hideResults() {
         $("#correct-holder").hide();
         $("#incorrect-holder").hide();
@@ -47,15 +61,22 @@ $(document).ready(function(){
         $("#restart-holder").hide();
     }
 
+
+    // Display the question and choices
+
     function displayQuestions(){
+
         hideResults();
-        $('#timer').show();
+        // $('#timer').show();
         $('#question-holder').html(questions[count]);
         $("#choice-1").html(firstChoice[count]);
         $("#choice-2").html(secondChoice[count]);
         $("#choice-3").html(thirdChoice[count]);
         $("#choice-4").html(fourthChoice[count]);
 
+
+        // Functions to change the choices font color
+        
         $("#choice-1").hover(function() {
             $(this).css("color", "blue");
         },
@@ -80,33 +101,84 @@ $(document).ready(function(){
         function(){
             $(this).css("color", "black");
         });
+
+
+    }   
+
+    // Listening for the answer selected
+
+    $('#choice-1').on('click', answerSelected)
+    $('#choice-2').on('click', answerSelected)
+    $('#choice-3').on('click', answerSelected)
+    $('#choice-4').on('click', answerSelected)
+
+
+    // Checking the user selected answer
+
+    function answerSelected() {
+
+        hideResults();  
+
+        if ($(this).text() === answer[count]){
+
+            hideGameContainers();
+            stopTime();
+            isSelected = true;
+            
+            $('#answer').show();
+            $('#answer-holder').html("Correct!")
+
+            correct++;
+            count++;
+        } else {
+
+            hideGameContainers();
+            stopTime();
+            isSelected = true;
+
+            $('#answer').show();
+            $('#answer-holder').html('Incorrect! Correct answer was: ' + answer[count]);
+
+            incorrect++;
+            count++;
+        }
+
     }
+
+    // Display image for correct 
+
+    // Display the time remaining 
 
     function displayTime() {
         time--;
         $("#timer-holder").html("Time remaining: " + time);
       
-            if(time <= 0) {
-                hideHolders();
+            if (time <= 0) {
+                hideGameContainers();
                 stopTime();
-                // $("#answer").show();
-                // $("#answer").html("Time is up! The answer is: " + answer[count]);
+                $("#answer").show();
+                $("#answer").html("Time is up! The answer is: " + answer[count]);
                 // // displayImage();
-                // unanswered++;
-                // // count++;
+                unanswered++;
+                count++;
                 // checkGameEnd();
             }
     }
+
+    // Start the countdown for the time remaining
 
     function startTime(){
         clearInterval(timer);
         timer = setInterval(displayTime, 1000);
     }
 
-    function stopTime(){
+    // Stop the countdown for the time remaining
+
+    function stopTime() {
         clearInterval(timer);
     }
 
+    // Start the game 
 
     function startGame(){
         
